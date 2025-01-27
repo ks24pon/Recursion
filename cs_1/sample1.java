3916fc65e848434b1b4e72fad2a251731a0458da
ks24pon <pom123re0624@gmail.com>
2025-01-27 20:16:56 +0900

    E表記

---
+++ cs_1/sample1.java
@@ -0,0 +1,10 @@
+class MyClass{
+    public static void main(String[] args){
+        // 小数出力
+        System.out.println(3.14);
+        // E表記出力
+        System.out.println((int)2e3);
+        // E表記出力
+        System.out.println(5e-2);
+    }
+}
