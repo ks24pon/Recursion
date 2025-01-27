67db84c7db7cba923a0971801663eebd7abfc209
ks24pon <pom123re0624@gmail.com>
2025-01-27 16:12:08 +0900

    文字列

---
+++ cs_1/sample.java
@@ -0,0 +1,12 @@
+class MyClass{
+    public static void main(String[] args){
+        // 文字列の出力
+        System.out.println("The earth was bluish.");
+        // 空文字列出力
+        System.out.println("");
+        // 整数の出力
+        System.out.println(1 + 1);
+        // 文字列1同士の出力
+        System.out.println("1" + "1");
+    }
+}
