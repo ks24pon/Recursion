430c2775c865ebfc9dde42cf94ccd15549e4da7d
ks24pon <pom123re0624@gmail.com>
2025-01-28 18:09:38 +0900

    リテラル

---
+++ cs_1/sample2.java
@@ -0,0 +1,14 @@
+class MyClass{
+    public static void main(String[] args){
+        // 文字
+        System.out.println('P');
+        // 整数
+        System.out.println(2022);
+        // 文字列
+        System.out.println("I am learning primitive data. #RecursionCS ");
+        // 小数
+        System.out.println(12.3);
+        // プール値
+        System.out.println(false);
+    }
+}
