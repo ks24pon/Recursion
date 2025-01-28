13971c26feea4c43df4d50dfdc9e3f04b7e09f4e
ks24pon <pom123re0624@gmail.com>
2025-01-28 18:47:56 +0900

    演算子の優先順位

---
+++ cs_1/sample3.java
@@ -0,0 +1,9 @@
+class MyClass{
+    public static void main(String[] args){
+        // 演算子
+        System.out.println(2 - 81 / 3 / 3 * 3 * 3 + 8);
+        System.out.println(3 + 10 / 2 * 5);
+        System.out.println(4 + 39 / (8 + 5) * 2);
+        System.out.println(5 + (80 + 1) / 9 / 3 * 5);
+    }
+}
